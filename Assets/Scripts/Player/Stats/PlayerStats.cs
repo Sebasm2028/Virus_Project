d3cab13de4fc8444639558390df506052645e89d
Camilo José de Jesús Amador Cabarcas <138091417+xMetroi@users.noreply.github.com>
2024-07-24 12:39:07 -0500

    Player Stats and Zombie Attack implemented

---
+++ Assets/Scripts/Player/Stats/PlayerStats.cs
@@ -0,0 +1,67 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStats : MonoBehaviour
+{
+    [Header("Health Properties")]
+    [SerializeField] private float healthPoints;
+
+    [Header("Weapons Properties")]
+    [SerializeField] private int ammoInCartridge;
+    [SerializeField] private int totalAmmo;
+
+    #region Events
+
+    public event Action<float> OnPlayerDamaged;
+
+    public event Action<int> OnPlayerGetsAmmo;
+
+    #endregion
+
+    #region Getter / Setter
+
+    public float GetHP() { return healthPoints; }
+
+    public void SetHP(float hp) { this.healthPoints = hp; }
+
+    public float GetAMMOInCartridge() { return ammoInCartridge; }
+
+    public void SetAmmoInCartrige(int ammoInCartrige) { this.ammoInCartridge = ammoInCartrige; }
+
+    public int GetTotalAmmo() { return totalAmmo; }
+
+    private void SetTotalAmmo(int totalAmmo) { this.totalAmmo = totalAmmo; }
+
+    #endregion
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    #region Health Points
+
+    public void DamagePlayer(float damage)
+    {
+        this.healthPoints -= damage;
+    }
+
+    #endregion
+
+    #region Ammo
+
+    public void AddAmmo(int ammo)
+    {
+        this.totalAmmo += ammo;
+    }
+
+    #endregion
+}
--- Assets/Scripts/Zombie/ZombieStats.cs
+++ Assets/Scripts/Zombie/ZombieStats.cs
@@ -7,14 +7,22 @@ public class ZombieStats : MonoBehaviour
 {
     [Header("Health Stats")]
     [SerializeField] private float healthPoints;
+    [SerializeField] private Transform attackPosition;
+    [SerializeField] private float attackRadius;
 
     [Header("Combat Stats")]
     [SerializeField] private float damagePoints;
     [SerializeField] private float attackSpeed;
+    [SerializeField] private bool canAttack = true;
+    [SerializeField] private LayerMask playerMask;
+
+    [Header("Script References")]
+    [SerializeField] private ZombieMovement zombieMovement;
 
     #region Eventos
 
     public event Action OnZombieDie;
+    public event Action OnZombieAttack;
 
     #endregion
 
@@ -28,6 +36,7 @@ public class ZombieStats : MonoBehaviour
     void Update()
     {
         HealthManager();
+        Attack();
     }
 
     #region Health
@@ -48,9 +57,46 @@ public class ZombieStats : MonoBehaviour
 
     #endregion
 
-    [ContextMenu("test")]
-    public void test()
+    #region Attack
+
+    private void Attack()
     {
-        Damage(5);
+        if (zombieMovement.ArrivedToPlayer() && canAttack)
+        {
+
+            Collider[] colliders = Physics.OverlapSphere(attackPosition.position, attackRadius, playerMask);
+
+            if (colliders != null)
+            {
+                if (colliders.Length > 0 &&colliders[0] != null)
+                {
+                    PlayerStats playerStats = colliders[0].gameObject.GetComponent<PlayerStats>();
+
+                    if (playerStats != null)
+                    {
+                        canAttack = false;
+                        OnZombieAttack?.Invoke();
+                        playerStats.DamagePlayer(damagePoints);
+                        StartCoroutine(attackCoroutine());
+                    }
+
+                }
+            }
+        }
+    }
+
+    private IEnumerator attackCoroutine()
+    {
+        yield return new WaitForSeconds(attackSpeed);
+
+        canAttack = true;
+    }
+
+    #endregion
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.white;
+        Gizmos.DrawWireSphere(attackPosition.position, attackRadius);
     }
 }
